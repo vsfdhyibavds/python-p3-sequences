47dc50e23f45a66908eaaf03e03505f29900f5cb
Eugene Wekesa <eugenco578@gmail.com>
2025-05-17 14:36:04 +0300

    "feat:looping through sequences"

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    """Prints a list of Fibonacci numbers of the given length."""
+    if length <= 0:
+        print([])
+        return
+    fib = [0, 1]
+    if length == 1:
+        print([0])
+        return
+    while len(fib) < length:
+        fib.append(fib[-1] + fib[-2])
+    print(fib[:length])
